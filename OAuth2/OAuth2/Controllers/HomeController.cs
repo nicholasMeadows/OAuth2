143adf2972721d1f2848d4105bd0212126d07c25
nicholasMeadows <meadowsnick98@gmail.com>
2018-06-13 21:13:45 -0500

    Refactored Index
    Login page fixed. Added register button, Added parameter validation, token generation.

--- OAuth2/OAuth2/Controllers/HomeController.cs
+++ OAuth2/OAuth2/Controllers/HomeController.cs
@@ -11,13 +11,16 @@ namespace OAuth2.Controllers
     public class HomeController : Controller
     {
         [RequireHttps]
+        [Route("/authorize")]
         public IActionResult Index(UserPassModel userPass, [FromQuery] ParamModel param)
         {
+            //Validate the parameters passed in the url
             string validationResposne = DatabaseContext.ValidateParams(param);
             if (!validationResposne.Equals("Valid")) {
                 return Ok(validationResposne);
             }
 
+            //Server side password validation
             if (userPass.username != null && userPass.password != null)
             {
                 if (userPass.password.Contains('\'') || userPass.password.Contains('/') || userPass.password.Contains('"'))
@@ -29,12 +32,13 @@ namespace OAuth2.Controllers
                 {
                     TempData["err"] = "Invalid";
                     return View();
-                }
+                }//Validates username and password 
                 else if (DatabaseContext.ValidateUser(userPass.username, userPass.password))
                 {
+                    //Generate request token
                     string request_token = DatabaseContext.GenerateToken();
-                    return Redirect("/" + param.redirect_uri);
-                    //return Redirect("http://www.google.com?code=" + request_token);
+                    //redirects user to redirect_url with request code
+                    return Redirect(param.redirect_uri+"?code=" + request_token);
                 }
                 else
                 {
@@ -42,59 +46,7 @@ namespace OAuth2.Controllers
                     return View();
                 }
             }
-
             return View();
-
-            //return RedirectToAction("Login", param);
-        }
-
-        [RequireHttps]
-        public IActionResult Login(UserPassModel userPass, [FromQuery] ParamModel param)
-        {
-            if (userPass.username != null)
-            {
-                return Ok(param.client_id);
-            }
-
-            return View("Index");
-        }
-            
-        }
+        }               
+    }
 }
-
-
-
-
-
-/*
-            [RequireHttps]
-            public IActionResult Login(UserPassModel userPass, [FromQuery] ParamModel param)
-            {
-                if (userPass.username != null && userPass.password != null)
-                {
-                    if (userPass.password.Contains('\'') || userPass.password.Contains('/') || userPass.password.Contains('"'))
-                    {
-                        TempData["err"] = "Invalid";
-                        return View("~/Views/Home/Index.cshtml");
-                    }
-                    else if (userPass.username.Contains('\'') || userPass.username.Contains('/') || userPass.username.Contains('"'))
-                    {
-                        TempData["err"] = "Invalid";
-                        return View("~/Views/Home/Index.cshtml");
-                    }
-                    else if (DatabaseContext.ValidateUser(userPass.username, userPass.password))
-                    {
-                        string request_token = DatabaseContext.GenerateToken();
-                        return Redirect("/" + param.redirect_uri);
-                        //return Redirect("http://www.google.com?code=" + request_token);
-                    }
-                    else
-                    {
-                        TempData["err"] = "Invalid";
-                        return View("~/Views/Home/Index.cshtml");
-                    }
-                }
-                TempData["err"] = "Invalid";
-                return View("~/Views/Home/Index.cshtml");
-
-            }*/
